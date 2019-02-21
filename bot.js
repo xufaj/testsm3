26c9201a816e76a4114ed1dd2b89722e43d6b04f
xufaj <45463607+xufaj@users.noreply.github.com>
2019-02-21 19:11:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : SAJED ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nhelp `,"http://twitch.tv/Death Shop")
+client.user.setGame(`1help `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
